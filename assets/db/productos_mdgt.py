68a2cd69ebaeb6784bc5bf301d3110d2b4fbccbc
Israel Martinez <mtzdgtlab@gmail.com>
2024-06-24 16:46:48 -0400

    ROBOTS

--- assets/db/productos_mdgt.py
+++ assets/db/productos_mdgt.py
@@ -13,11 +13,6 @@ env = Environment(
 # Cargar la plantilla
 template = env.get_template("plantilla.html")
 
-# Verificar si las columnas de variantes existen
-has_size = 'size' in df.columns
-has_quantity = 'quantity' in df.columns
-has_price = 'price' in df.columns
-
 # Iterar sobre cada fila del DataFrame
 product_data = None
 
@@ -32,7 +27,6 @@ for index, row in df.iterrows():
             print(f"Archivo generado: {product_data['page_title']}.html")
         
         product_data = row.fillna('').to_dict()
-        product_data['variants'] = []
 
         # Verifica si los campos críticos están presentes y no son NaN
         required_fields = ['name', 'metad', 'productt', 'category', 'subcategory', 'img1', 'secalt', 'description']
@@ -40,19 +34,6 @@ for index, row in df.iterrows():
             print(f"Faltan datos críticos en el índice {index}, se omite este registro.")
             product_data = None
             continue
-
-    # Si 'name' es NaN, es una línea de variante
-    elif product_data is not None:
-        variant = {}
-        if has_size:
-            variant['size'] = row['size']
-        if has_quantity:
-            variant['quantity'] = row['quantity']
-        if has_price:
-            variant['price'] = row['price']
-        
-        if variant:
-            product_data['variants'].append(variant)
 
 # Renderizar el último producto si existe
 if product_data is not None:
